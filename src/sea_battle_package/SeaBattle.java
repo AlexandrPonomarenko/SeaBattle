dfb6837ef422670d7f884f05641d8592578efdfd
Alex <furriets@gmail.com>
2017-07-01 21:27:20 +0300

    create new start page

--- src/sea_battle_package/SeaBattle.java
+++ src/sea_battle_package/SeaBattle.java
@@ -1,11 +1,24 @@
 package sea_battle_package;
 
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Alexandr on 01.07.2017.
  */
 public class SeaBattle {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run()
+            {
+                JFrame frame = new Loader();
+                frame.setTitle("Sea Battle");
+                frame.setVisible(true);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            }
+        });
+
     }
 }
