292360876ccd316a55b52652c6d875cbf8e3c697
Alex <furriets@gmail.com>
2017-07-01 14:48:52 +0300

    New project Sea Battle

---
+++ src/sea_battle_package/SeaBattle.java
@@ -0,0 +1,11 @@
+package sea_battle_package;
+
+
+/**
+ * Created by Alexandr on 01.07.2017.
+ */
+public class SeaBattle {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
